792016ddc996fa0196a07a21a5428e31ccc24782
Aiman__ <93353505+Rizouke@users.noreply.github.com>
2023-10-16 01:10:52 +0200

    Update listepersonne.blade.php

--- listepersonne.blade.php
+++ listepersonne.blade.php
@@ -1,7 +1,7 @@
-@extends ('modeles/visiteur')
+@extends('sommaire')
     @section('contenu1')
-        <div class="container text-center">
-            <div class="row">
+        <div class="container text-center contenu" id="contenu">
+            <div class=" corpsForm row">
                 <div class="col">
                     <h1>liste des visiteurs</h1>
                     <table class="table">
@@ -10,6 +10,8 @@
                                 <th>id</th>
                                 <th>Nom</th>
                                 <th>Prenom</th>
+                                <th>Login</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -18,15 +20,18 @@
                                 <td>{{$unliste['id']}}</td>
                                 <td>{{$unliste['nom']}}</td>
                                 <td>{{$unliste['prenom']}}</td>
+                                <td>{{$unliste['login']}}</td>
                                 <td>
-                                    <a href="" class="btn btn-primary">Modifier</a>
-                                    <a href="" class="btn btn-info">Supprimer</a>
+                                    <a href="{{Route('modifieruser', ['id'=>$unliste['id']])}}" class="btn btn-primary">Modifier</a>
+                                    <a href="{{Route('genePdf',['id'=>$unliste['id']])}}" class="btn btn-info">Générer un état en pdf</a>
+                                    <a href="{{Route('supprimeruser',['id'=>$unliste['id']])}}" class="btn btn-info">Supprimer</a>
                                 </td>
                             </tr>
                             @endforeach
                         </tbody>
                         </table><hr>
-                        <a href="" class="btn btn-secondary">Ajouter un utilisateur</a>
-                        
-
-@endsection
+                        <a href="{{Route('ajouteruser')}}" class="btn btn-secondary">Ajouter un utilisateur</a>
+                </div> 
+            </div>       
+        </div>
+    @endsection
