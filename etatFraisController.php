3e671d0539a160a20075d4fa45eaac12753d1f8b
Aiman__ <93353505+Rizouke@users.noreply.github.com>
2023-10-16 01:06:56 +0200

    Update etatFraisController.php

--- etatFraisController.php
+++ etatFraisController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use PdoGsb;
 use MyDate;
+use PDF;
 class etatFraisController extends Controller
 {
     function selectionnerMois(){
@@ -68,11 +69,127 @@ class etatFraisController extends Controller
     }
 
     function listePersonne(){
+        //cette fonction permet d'afficher la liste des visiteurs inscrits dans la BDD
         if(session('visiteur')!=null){
+            $visiteur = session('visiteur');
             $liste=Pdogsb::Listepersonne();
-            return view('listepersonne') ->with('liste', $liste);
+            return view('listepersonne') ->with('liste', $liste)
+            ->with('visiteur',$visiteur);
         }else{
             return view('connexion') ->with('erreurs', null);
         }
     }
+     function suppruser(Request $request){
+        //cette fonction va permettre de supprimer un utilisateur de la BDD
+        if(session('visiteur')!=null){
+            $visiteur = session('visiteur');
+            $id=$request['id'];
+            $req=Pdogsb::supprimerUser($id);
+            $liste=Pdogsb::listePersonne();
+            return view('listepersonne') ->with('liste', $liste) ->with('visiteur',$visiteur);
+
+        }else{
+            return view('connexion') ->with('erreurs', null);
+        }
+     }
+     function selectionneruser(Request $request){
+        //cette fonction permet de trouver un utilisateur dans la BDD grâce à son id
+        //elle va permettre pour une inscription de vérifier si un utilisateur
+        //avec le même id existe
+        if(session('visiteur')!=null){
+
+            $visiteur = session('visiteur');
+
+            $id=$request['id'];
+            //dd($id);
+            $liste=Pdogsb::selectionneruser($id);
+            return view('formmodif')->with('liste',$liste)
+                    ->with('visiteur', $visiteur);
+        }else{
+            return view('connexion') ->with('erreurs', null);
+        }
+     }
+
+     function ajouterUtilisateur(Request $request){
+        if(session('visiteur')!=null){
+
+            $visiteur = session('visiteur');
+            //dd($visiteur);
+            $id=$request['id'];
+            $login=$request['login'];
+            $mdp=$request['mdp'];
+            $nom=$request['nom'];
+            $prenom=$request['prenom'];
+            $adresse=$request['adresse'];
+            $ville=$request['ville'];
+            $cp=$request['cp'];
+            $date=$request['date'];
+            $test=Pdogsb::selectionneruser($id);
+            if(empty($test)){
+                //verification de si l'utilisateur existe
+                //s'il est pas existant on peut donc le créer
+                $req=Pdogsb::ajouter($id,$nom,$prenom,$login,$mdp,$adresse,$cp,$ville,$date);
+                $liste=Pdogsb::listePersonne();
+                return view('listepersonne') ->with('liste', $liste) ->with('visiteur',$visiteur);
+            }
+            else{
+                //si le visiteur est déja crée, l'utilisateur va être renvoyé au formulaire d'ajout
+                return view('form_ajout')->with('visiteur',$visiteur);
+            }
+            
+        }else{
+            return view('connexion') ->with('erreurs', null);
+        }
+     }
+
+     function modifierUser(Request $request){
+        if(session('visiteur')!=null){
+            //fonction qui récup les valeurs du formulaire et modifie en conséquence le visiteur
+            $visiteur = session('visiteur');
+            $id=$request['id'];
+            $login=$request['login'];
+            $mdp=$request['mdp'];
+            $nom=$request['nom'];
+            $prenom=$request['prenom'];
+            $adresse=$request['adresse'];
+            $ville=$request['ville'];
+            $cp=$request['cp'];
+            $date=$request['date'];
+            $req=Pdogsb::modifierUser($id,$nom,$prenom,$login,$adresse,$cp,$ville,$date,$mdp);
+            $liste=Pdogsb::Listepersonne();
+            return view('listepersonne') ->with('liste', $liste) ->with('visiteur',$visiteur)
+            ;
+        }else{
+            return view('connexion') ->with('erreurs', null);
+        }
+     }
+
+    function ajoutUser(){
+        //cette fonction fait le lien entre la route et la vue du formulaire d'ajout
+        if (session('visiteur')!= null){
+            $visiteur = session('visiteur');
+            
+            return view('form_ajout')->with('visiteur',$visiteur);
+        }else{
+            return view('connexion') ->with('erreurs', null);
+        }
+     }
+
+     function genererEtat(){
+        if (session('visiteur')!= null){
+            $visiteur = session('visiteur');
+            $liste=Pdo::genererEtat($id);
+            return view('form_ajout')->with('visiteur',$visiteur);
+        }else{
+            return view('connexion') ->with('erreurs', null);
+        }
+     }
+
+     function generationid(){
+        
+     }
 }
+
+		   
+         
+      
