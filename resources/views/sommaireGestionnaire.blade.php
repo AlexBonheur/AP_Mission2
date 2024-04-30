35c5bd8846b96f1bf83ea7a9e190a38e97c16126
AlexBonheur <123744707+AlexBonheur@users.noreply.github.com>
2024-04-30 08:26:52 +0200

    Add files via upload

---
+++ resources/views/sommaireGestionnaire.blade.php
@@ -0,0 +1,31 @@
+@extends ('modeles/visiteur')
+@section('menu')
+    <!-- Division pour le sommaire -->
+    <div id="menuGauche">
+        <div id="infosUtil">
+
+        </div>
+        <ul id="menuList">
+            <li >
+                <strong>Bonjour {{ $gestionnaire['nom'] . ' ' . $gestionnaire['prenom'] }}</strong>
+
+            </li>
+            <li class="smenu">
+                <a href="{{ route('chemin_gestionFrais')}}" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+            </li>
+            <li class="smenu">
+                <a href="{{ route('chemin_selectionMois') }}" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+            </li>
+            <li class="smenu">
+                <a href="{{ route('chemin_test')}}" title="test">1</a>
+            </li>
+            <li class="smenu">
+                <a href="{{ route('chemin_deconnexion') }}" title="Se déconnecter">Déconnexion</a>
+            </li>
+            <li class="smenu">
+                <a href="{{ route('listepersonne') }}" title="Se déconnecter">Liste des utilisateurs</a>
+            </li>
+        </ul>
+
+    </div>
+@endsection
